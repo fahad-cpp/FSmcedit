1376bf70751e6ea4b816bb6357d2d850df85373e
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-04-03 16:26:07 +0530

    Initial Commit

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+#include <leveldb/db.h>
+
+int main(){
+    
+}
---
+++ build.bat
@@ -0,0 +1,7 @@
+if not exist build mkdir build
+pushd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake --build . --config Release --parallel
+popd
+
+build\FSmcedit.exe > world_data.txt
