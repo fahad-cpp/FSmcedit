a05f21060ea3dfec7e395f9a2fa28d7ffea64fe5
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-04-05 15:55:49 +0530

    Setup for Leveldb access

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,23 @@
 #include <leveldb/db.h>
+#include <iostream>
 #include "NBT.h"
 
 int main(){
     leveldb::DB* db;
-    //TODO:Handle leveldb
+    leveldb::Options options;
+    options.create_if_missing = false;
+    leveldb::Status status = leveldb::DB::Open(options,"tmp/testdb",&db);
+    if(!status.ok()){
+        std::cerr << status.ToString() << "\n";
+        return 1;
+    }
+        
+    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    for(it->SeekToFirst();it->Valid();it->Next()){
+        std::cout << (it->key().ToString()) << "\n";
+    }
+
+    delete it;
     delete db;
+    return 0;
 }
