ab4f3450f30f335b2ab346143c3ae8035a8557d3
Fahad704 <shaikh.fahad.77377@gmail.com>
2026-04-21 21:40:54 +0530

    Fix leveldb-mcpe not working in Non-vs projects

--- build.bat
+++ build.bat
@@ -3,9 +3,9 @@ if not exist build mkdir build
 if exist build\Release\FSmcedit.exe del build\Release\FSmcedit.exe
 pushd build
 if "%1" == "clean" del CMakeCache.txt
-if not exist CMakeCache.txt cmake .. -G "Visual Studio 18 2026" -DCMAKE_BUILD_TYPE=Release
+if not exist CMakeCache.txt cmake .. -G "Ninja" -DCMAKE_BUILD_TYPE=Release
 cmake --build . --config Release --parallel
 popd
 if exist worldData del /f /q "worldData"
 if not exist worldData mkdir worldData
-build\Release\FSmcedit.exe
+build\FSmcedit.exe
