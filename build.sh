ab94a9220441801b4519c0fa6cc99dcc0a5854ce
= <shaikh.fahad.77377@gmail.com>
2026-05-14 13:15:48 +0530

    Fix merge conflict

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
+if [ "$1" == "clean" ]; then
+    rm -rf build
+fi
 mkdir -p build
 cd build
-if [ "$1" == "clean" ]; then
-    rm -f CMakeCache.txt
-fi
-[ ! -f CMakeCache.txt ] && cmake -S .. -DCMAKE_BUILD_TYPE="Release"
+[ ! -f CMakeCache.txt ] && cmake -S .. -G "Ninja" -DCMAKE_CXX_COMPILER="clang++" -DCMAKE_BUILD_TYPE="Release"
 cmake --build . --config Release --parallel
 cd ..
 mkdir -p "worldData"
